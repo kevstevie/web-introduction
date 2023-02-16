7ec6f6d448b6bf46ba1d13d1315a9d242d54c1b1
kevstevie <jianna442@gmail.com>
2023-02-16 15:05:07 +0900

    자기 소개 작성

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var submit = document.getElementById("submit");
+
+submit.onclick = function() {
+    var name = document.getElementById("username");
+    var checkboxes = document.getElementsByName("like");
+    var count = 0;
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            count += 1;
+        }
+    }
+
+    alert(name.value + "님 저와 " + count + "개의 취향이 같으시네요!");
+};
+
+var clear = document.getElementById("clear");
+clear.onclick = function() {
+    // checkbox 전부 클리어
+    var checkboxes = document.getElementsByName("like");
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = false;
+    }
+};
